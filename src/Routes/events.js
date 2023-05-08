36ba9cfba8deecf1164caff6a3eee5594b21908a
Facutorress <86375929+Facutorress@users.noreply.github.com>
2023-05-08 17:37:39 -0300

    rutas putevent y geteventbyname

--- src/Routes/events.js
+++ src/Routes/events.js
@@ -3,11 +3,13 @@ const getEvents = require("../Controllers/GetEvents");
 const getEventByName= require("../Controllers/GetEventByName")
 const createEvents = require("../Controllers/createEvents");
 const getEventById = require("../Controllers/GetEventById");
+const putEvents = require("../Controllers/PutEvents")
 const router = Router();
 
 router.get("/", getEvents);
 router.get("/:id", getEventById);
 router.post("/eventcreate", createEvents);
 router.put("/:id", putEvents);
+router.get("/name", getEventByName);
 
 module.exports = router;
