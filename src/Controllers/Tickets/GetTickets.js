6c2903196040b77d2d57dcaa5ca38d35d9378a7b
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-05-09 13:02:32 -0300

    Tickets create get routes

---
+++ src/Controllers/Tickets/GetTickets.js
@@ -0,0 +1,12 @@
+const { Tickets } = require("../../db");
+
+const getTickets = async (req, res) => {
+  try {
+    const tickets = await Tickets.findAll();
+    res.status(200).json(tickets);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = getTickets;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const { Router } = require("express");
 const events=require('./events')
 const users=require('./users')
 const router = Router();
+const tickets = require("./tickets");
 
 router.use("/events", events);
 router.use("/users", users);
+router.use("/tickets", tickets);
 
 module.exports = router;
