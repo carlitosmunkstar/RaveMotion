f6c0b4c562e23d8645d7c6b9329a736fbb0556bc
FacuCasado <facufcasado@gmail.com>
2023-05-10 16:43:43 -0300

    Cambios en PP

--- src/Routes/tickets.js
+++ src/Routes/tickets.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
-const PostUserTickets = require("../Controllers/Tickets/PostUserTickets");
-const getUserTickets = require("../Controllers/Tickets/GetUserTickets");
+const PostUserTickets = require("../Controllers/UserTickets/PostUserTickets");
+const getUserTickets = require("../Controllers/UserTickets/GetUserTickets");
 const getTickets = require("../Controllers/Tickets/GetTickets");
 const createTickets = require("../Controllers/Tickets/CreateTickets");
 const router = Router();
