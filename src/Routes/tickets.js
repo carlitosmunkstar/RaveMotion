31765385b4e437b3c508a960eb1da49d361f90c4
carlitosmunkstar <camobaan@gmail.com>
2023-05-10 16:54:29 -0500

    ticketUserById y ticketEventById

--- src/Routes/tickets.js
+++ src/Routes/tickets.js
@@ -3,12 +3,15 @@ const PostUserTickets = require("../Controllers/UserTickets/PostUserTickets");
 const getUserTickets = require("../Controllers/UserTickets/GetUserTickets");
 const getTickets = require("../Controllers/Tickets/GetTickets");
 const createTickets = require("../Controllers/Tickets/CreateTickets");
+const getTicketsById = require("../Controllers/Tickets/GetTicketsById");
+const getUserTicketsByID = require("../Controllers/UserTickets/GetUserTicketsById");
 const router = Router();
 
-
 router.get("/", getTickets);
-router.get("/userTickets", getUserTickets)
-router.post("/newUserTickets", PostUserTickets)
+router.get("/ticketsByEvent/:id", getTicketsById);
+router.get("/ticketsByUser/:id", getUserTicketsByID);
+router.get("/userTickets", getUserTickets);
+router.post("/newUserTickets", PostUserTickets);
 router.post("/createtickets", createTickets);
 
 module.exports = router;
