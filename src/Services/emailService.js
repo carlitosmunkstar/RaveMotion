8dd45d28747ac01e14eedf8687b6f6285d4cac3b
Facutorress <86375929+Facutorress@users.noreply.github.com>
2023-05-10 00:32:04 -0300

    reset password

--- src/Services/emailService.js
+++ src/Services/emailService.js
@@ -2,24 +2,24 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (recipient, subject, message) => {
   try {
-    // Configura las opciones de transporte
+
     const transporter = nodemailer.createTransport({
-      service: 'gmail', // Usa el servicio de Gmail
+      service: 'gmail', 
       auth: {
-        user: "torresfacundo.lt@gmail.com", // Reemplaza con tu correo electrónico de Gmail
-        pass: "cwbocpbsnimnijhj", // Reemplaza con la contraseña de tu correo electrónico de Gmail
+        user: "torresfacundo.lt@gmail.com",
+        pass: "cwbocpbsnimnijhj",
       },
     });
 
-    // Configura las opciones de correo electrónico
+  
     const mailOptions = {
-      from: "your-email@example.com", // Reemplaza con tu correo electrónico de Gmail
+      from: "your-email@example.com", 
       to: recipient,
       subject: subject,
       text: message,
     };
 
-    // Enviar el correo electrónico
+    
     await transporter.sendMail(mailOptions);
     console.log("Email enviado");
   } catch (error) {
