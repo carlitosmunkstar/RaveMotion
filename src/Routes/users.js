94d0218b541da08bd2b366b338e829ae1cf9a047
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-05-13 19:14:24 -0300

    user sign up rute fix

--- src/Routes/users.js
+++ src/Routes/users.js
@@ -1,15 +1,15 @@
 const { Router } = require("express");
 const singUp = require("../Controllers/Users/SingUp");
 const singIn = require("../Controllers/Users/SingIn");
-const  ResetPassword  = require("../Controllers/Users/ResetPassword");
-const ConfirmResetPassword= require("../Controllers/Users/ConfirmResetPassword")
-const signOut= require("../Controllers/Users/SignOut")
+const ResetPassword = require("../Controllers/Users/ResetPassword");
+const ConfirmResetPassword = require("../Controllers/Users/ConfirmResetPassword");
+const signOut = require("../Controllers/Users/SignOut");
 const router = Router();
 
-router.get("/logout",signOut);
+router.get("/logout", signOut);
 router.get("/login", singIn);
-router.get("/resetpassword",ResetPassword)
-router.get("/resetpassword/:resetPasswordToken",ConfirmResetPassword)
-router.post("/singUp", singUp);
+router.get("/resetpassword", ResetPassword);
+router.get("/resetpassword/:resetPasswordToken", ConfirmResetPassword);
+router.post("/signup", singUp);
 
 module.exports = router;
