b473462002c765f1ac428fce09b59101a768324d
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-05-14 17:20:50 -0300

    db url config

--- src/db.js
+++ src/db.js
@@ -4,14 +4,14 @@ const fs = require("fs");
 const path = require("path");
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE } = process.env;
+const DB_URL =
+    process.env.DB_URL ||
+    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
-  {
+const sequelize = new Sequelize(DB_URL, {
     logging: false,
     native: false,
-  }
-);
+});
 
 const basename = path.basename(__filename);
 
@@ -21,23 +21,24 @@ const modelDefiners = [];
 //* hace un require a todos los archivos adentro de la carpeta Models
 //* este fragmento de codigo lo saque de db.js que nos dieron en el pi
 fs.readdirSync(path.join(__dirname, "/Models"))
-  .filter(
-    (file) =>
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-  )
-  .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, "/Models", file)));
-  });
+    .filter(
+        (file) =>
+            file.indexOf(".") !== 0 &&
+            file !== basename &&
+            file.slice(-3) === ".js"
+    )
+    .forEach((file) => {
+        modelDefiners.push(require(path.join(__dirname, "/Models", file)));
+    });
 // Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [
-  entry[0][0].toUpperCase() + entry[0].slice(1),
-  entry[1],
+    entry[0][0].toUpperCase() + entry[0].slice(1),
+    entry[1],
 ]);
 sequelize.models = Object.fromEntries(capsEntries);
-
 
 const { Event, User, Ticket, TicketsSold } = sequelize.models;
 
@@ -46,45 +47,43 @@ const { Event, User, Ticket, TicketsSold } = sequelize.models;
 //? pero ciertos tickets solo pueden pertenecer a un solo usuario
 
 //todo Relación uno a varios entre Tickes y TicketsSold
-Ticket.hasMany(TicketsSold)
+Ticket.hasMany(TicketsSold);
 TicketsSold.belongsTo(Ticket, {
-  foreignKey:"ticketId",
-})
+    foreignKey: "ticketId",
+});
 
 //todo Relación uno a varios entre User(comprador) y TicketsSold
 User.hasMany(TicketsSold);
 TicketsSold.belongsTo(User, {
-  foreignKey: "userId",
+    foreignKey: "userId",
 });
 
 //todo Relación uno a varios entre Event y Tickets
 Event.hasMany(Ticket, {
-  foreignKey: "eventId",
-  onDelete: "CASCADE", //? cuando se elimina un evento se eliminan todos los tickes asociados
+    foreignKey: "eventId",
+    onDelete: "CASCADE", //? cuando se elimina un evento se eliminan todos los tickes asociados
 });
 Ticket.belongsTo(Event, {
-  foreignKey: "eventId",
+    foreignKey: "eventId",
 });
 
-//todo 
+//todo
 Event.hasMany(TicketsSold);
 TicketsSold.belongsTo(Event, {
-  foreignKey: "eventId",
+    foreignKey: "eventId",
 });
 
 //todo Relación uno a varios entre User(productora) y Event
 User.hasMany(Event);
 Event.belongsTo(User, {
-  foreignKey: "userId",
-})
+    foreignKey: "userId",
+});
 
 //todo Relación varios a varios entre Tickets y User(comprador) a travez de la tabla ticketsSold
 // Ticket.belongsToMany(User, { through: TicketsSold });
 // User.belongsToMany(Ticket, { through: TicketsSold });
 
-
-
 module.exports = {
-  sequelize,
-  ...sequelize.models,
+    sequelize,
+    ...sequelize.models,
 };
