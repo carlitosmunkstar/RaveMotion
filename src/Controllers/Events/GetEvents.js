a802575bc4bf2d4e72e7fd42591acf292189dfbb
FacuCasado <facufcasado@gmail.com>
2023-05-16 02:14:16 -0300

    Borrado Logico

--- src/Controllers/Events/GetEvents.js
+++ src/Controllers/Events/GetEvents.js
@@ -2,7 +2,7 @@ const {Event, Ticket}=require('../../db.js')
 
 const getEvents=async(req,res)=>{
     try {
-        const events=await Event.findAll()
+        const events=await Event.findAll({where:{status:true}})
         res.status(200).json(events)
     } catch (error) {
         res.status(500).json({error:error.message});
