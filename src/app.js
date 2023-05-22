a2d790eadf637873fadc7605aff623f82d188dc0
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-05-22 07:53:33 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const routes = require("./Routes/index");
+const routes = require("./routes/index.js");
 const cors = require("cors");
 
 const CLIENT_ID = process.env.CLIENT_URL;
