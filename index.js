ffe02c66d85e68685a783d3b84f84736f249ade6
carlitosmunkstar <camobaan@gmail.com>
2023-05-23 20:30:43 -0500

    nuevos cambios a petición de GPT

---
+++ index.js
@@ -0,0 +1,9 @@
+const server = require("./app.js");
+const { sequelize } = require("./src/db.js");
+const APP_PORT = process.env.APP_PORT || 3001;
+
+sequelize.sync({force: false}).then(() => {
+  server.listen(APP_PORT, () => {
+    console.log(`Listen at ${APP_PORT}`);
+  });
+});
