e14cf4a710d31dacb5e1cd890c19a18569e2b776
FacuCasado <facufcasado@gmail.com>
2023-05-25 13:51:38 -0300

    time stamps

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { sequelize } = require("./src/db.js");
 const APP_PORT = process.env.APP_PORT || 3001;
 
-sequelize.sync({force: false}).then(() => {
+sequelize.sync({alter: true}).then(() => {
   server.listen(APP_PORT, () => {
     console.log(`Listen at ${APP_PORT}`);
   });
