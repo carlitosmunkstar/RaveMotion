8ea0a4c6c9d44b5a505c1bc6206e7a035f928ad8
carlos mosquera <75375613+carlitosmunkstar@users.noreply.github.com>
2023-05-25 10:42:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const server = require("./src/app")
 const { sequelize } = require("./src/db.js");
-const APP_PORT = process.env.APP_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 sequelize.sync({force: false}).then(() => {
-  server.listen(APP_PORT, () => {
-    console.log(`Listen at ${APP_PORT}`);
+  server.listen(PORT, () => {
+    console.log(`Listen at ${PORT}`);
   });
 });
