7cacbb6758fbc62fa0796bdd20aeefb234cd8531
Facutorress <86375929+Facutorress@users.noreply.github.com>
2023-05-30 17:37:41 -0300

    lol

--- src/Controllers/Events/RateEvent.js
+++ src/Controllers/Events/RateEvent.js
@@ -1,9 +1,11 @@
 const {Event,TicketsSold}=require('../../db')
 const {Op}=require('sequelize')
-const rateEvent=async(req,res)=>{
-    const{userId,id,rating}=req.body;
+
+const rateEvent = async(req,res) => {
+    const {userId,id,rating} = req.body;
+
     try {
-        const isBuyer=await TicketsSold.findAll({
+        const isBuyer = await TicketsSold.findAll({
             where:{
                 [Op.and]: [
                     { userId: userId },
@@ -12,8 +14,8 @@ const rateEvent=async(req,res)=>{
             }
         });
         if(isBuyer.length){
-            const rate=await Event.findByPk(id);
-            rate.rating=rate.rating+rating;
+            const rate = await Event.findByPk(id);
+            rate.rating = rate.rating + rating;
             rate.ricardo++;
             await rate.save();
             res.status(200).json(rate);
@@ -21,10 +23,9 @@ const rateEvent=async(req,res)=>{
             res.status(400).json('El usuario no compro entradas para este evento')
         }
     } catch (error) {
-        res.status(500).json({error:error.message})
+        console.error("Caught an error:", error);  // This will log the full error, not just the message
+        res.status(500).json({error: error.message})
     }
-
-
 }
 
 module.exports=rateEvent;
