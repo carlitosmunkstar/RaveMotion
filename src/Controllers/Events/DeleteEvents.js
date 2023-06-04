47cd724a36b3516f297a96e99a69b609b4c7a77f
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-06-03 21:38:05 -0300

    pretier

--- src/Controllers/Events/DeleteEvents.js
+++ src/Controllers/Events/DeleteEvents.js
@@ -1,17 +1,17 @@
-const {Event} = require('../../db');
+const { Event } = require("../../db");
 
 const deleteEvents = async (req, res) => {
-  try {
-    const {id} = req.params;
-    const deleted = await Event.findByPk(id);
-    if (!deleted) {
-      return res.status(400).json('Evento no encontrado');
+    try {
+        const { id } = req.params;
+        const deleted = await Event.findByPk(id);
+        if (!deleted) {
+            return res.status(400).json("Evento no encontrado");
+        }
+        deleted.status = false;
+        await deleted.save();
+        res.status(200).json(deleted);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
-    deleted.status=false;
-    await deleted.save();
-    res.status(200).json(deleted);
-  } catch (error) {
-    res.status(500).json({error: error.message});
-  }
 };
 module.exports = deleteEvents;
